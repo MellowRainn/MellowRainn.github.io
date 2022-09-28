034f9cc7b7b047d7dba512fa1a0af55f44773ad2
MellowRainn <ruij0036@hz.nl>
2022-09-28 23:46:09 +0200

    wut

--- js/navbar.js
+++ js/navbar.js
@@ -1,9 +1,11 @@
-/* Set the width of the side navigation to 250px */
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginRight = "250px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
   }
   
-  /* Set the width of the side navigation to 0 */
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
-  } 
+    document.getElementById("main").style.marginRight= "0";
+    document.body.style.backgroundColor = "white";
+  }
