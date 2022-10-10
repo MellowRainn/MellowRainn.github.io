b7041195d2ee065a8e3fddcbcf4a248e3ace03f3
MellowRainn <ruij0036@hz.nl>
2022-10-10 14:36:17 +0200

    pretty much done

--- js/quiz.js
+++ js/quiz.js
@@ -1,30 +1,30 @@
-function submitAnswers(){
-    let total = 2;
-    let score = 0;
-    let q1 = document.forms["quizForm"] ["q1"].value;
-    let q2 = document.forms["quizForm"] ["q2"].value;
+function submitAnswers() {
+  let total = 2;
+  let score = 0;
+  let q1 = document.forms["quizForm"]["q1"].value;
+  let q2 = document.forms["quizForm"]["q2"].value;
 
-    
-  for(let i = 1; i<= total; i++){
-          if( eval('q' + i) == null || eval('q' + i) == ''){
-        alert("you missed question " + i);
-        return false;
+
+  for (let i = 1; i <= total; i++) {
+    if (eval('q' + i) == null || eval('q' + i) == '') {
+      alert("you missed question " + i);
+      return false;
     }
   }
-    
-    let answers = ["a", "d"]
-    
-    for(let i = 1; i<= total; i++){
-        if(eval('q' + i) == answers [i - 1]){
-            score++;
-        }
+
+  let answers = ["a", "d"]
+
+  for (let i = 1; i <= total; i++) {
+    if (eval('q' + i) == answers[i - 1]) {
+      score++;
     }
-      alert('You scored ' + score + " out of " + total);
-    //display results
-    let results = document.getElementById('results');
-    results.innerHTML= '<h3> You scored <span' + score + '</span> out of <span>' + total + '</span></h3>';
-    
-  
-    
-    return false;
+  }
+  alert('You scored ' + score + " out of " + total);
+  //display results
+  let results = document.getElementById('results');
+  results.innerHTML = '<h3> You scored <span>' + score + '</span> out of <span>' + total + '</span></h3>';
+
+
+
+  return false;
 }
